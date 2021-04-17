416d1825432c9a64ed0394f30133ba57eb846174
abhiedward001 <iam001abhi@gmail.com>
2021-04-17 20:02:21 +0530

    my first react app basic build

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div>Hello i am abhiedwar001! This is my first react app</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
